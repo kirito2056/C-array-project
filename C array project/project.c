02314150f795cfa890c568a18d514a2466e2fbdc
kirito2056 <hajin_2056@naver.com>
2023-03-08 15:46:09 +0900

    .

--- C array project/project.c
+++ C array project/project.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
 
 int main() {
-	int temp;
-	int arr[10], b;
-	for (int i = 0; i < 10; i++) scanf("%d", &arr[i]);
-	scanf("%d", &b);
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (arr[i] > arr[j]) {
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
-	printf("%d", arr[b]);
+	int a, b, c;
+	scanf("%d %d", &a, &b); // %f %c %s
+	c = a + b;
 
+	return 0;
 }
